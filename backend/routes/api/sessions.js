ca7a1470d781e02efc19e8d3113058fda70a151e
Lucas <lucascostamonteiro@yahoo.com>
2022-02-03 13:14:13 -0800

    user log in/log out

--- backend/routes/api/sessions.js
+++ backend/routes/api/sessions.js
@@ -5,6 +5,7 @@ const { User } = require('../../db/models');
 
 const router = express.Router();
 
+// log in
 router.post('/', asyncHandler(async (req, res, next) => {
     const { credential, password } = req.body;
 
@@ -24,6 +25,11 @@ router.post('/', asyncHandler(async (req, res, next) => {
 })
 );
 
+// log out
+router.delete('/', (_req, res) => {
+    res.clearCookie('token');
+    return res.json({ message: 'success' });
+}
+);
 
 module.exports = router;
-
