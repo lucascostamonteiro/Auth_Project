bbd3009094a063f68e5dce5cc8ede01d516d6079
Lucas <lucascostamonteiro@yahoo.com>
2022-02-07 21:24:51 -0800

    bread crumbs

---
+++ backend/routes/api/albums.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+const { check } = require('express-validator');
+const { Album } = require('../../db/models/album');
+
+const router = express.Router();
+
+
+// TODO VALIDATION MIDDLEWARE
+
+// TODO post an album
+
+// TODO get albums associated with user
+
+// TODO edit a title of an album
+
+// TODO delete an album and its images
+
+
+
+module.exports = router;
---
+++ backend/routes/api/comments.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+const { check } = require('express-validator');
+const { Comments } = require('../../db/models/comment');
+
+const router = express.Router();
+
+// TODO VALIDATION MIDDLEWARE
+
+// TODO post comments
+
+// TODO get comments for a specific image
+
+// TODO edit a comment
+
+// TODO delete a comment
+
+
+
+module.exports = router;
