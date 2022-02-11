8fb3dc510458aa73928ca9147e259ac5ac525dc4
Lucas <lucascostamonteiro@yahoo.com>
2022-02-11 15:23:01 -0800

    comment sec

--- backend/routes/api/comments.js
+++ backend/routes/api/comments.js
@@ -2,9 +2,8 @@ const express = require('express');
 const asyncHandler = require('express-async-handler');
 const { check } = require('express-validator');
 const { requireAuth } = require('../../utils/auth')
-const { Comment } = require('../../db/models/comment');
-const { Image } = require('../../db/models/image')
-const { User } = require('../../db/models/user')
+const { Comment, User } = require('../../db/models/')
+
 
 const router = express.Router();
 
@@ -13,27 +12,32 @@ const commentValidation = [
     check('comment')
         .exists({ checkFalsy: true })
         .isLength({ min: 1 })
-        .withMessage('Comment cannot be empty'),
+        .withMessage('Comments cannot be empty'),
 ]
 
 // TODO get comments for a specific image
-router.get('/', asyncHandler(async (req, res) => {
-    const comments = Comment.findAll({ where: imageId });
-    console.log('+++CMT+++', comments)
-    res.json({ comments });
+router.get('/:imageId', asyncHandler(async (req, res) => {
+    const { imageId } = req.params;
+    const comments = await Comment.findAll({ where: { imageId }, include: User });
+    res.json(comments);
 }));
 
 
 
 // TODO post comments
+router.post('/', commentValidation, asyncHandler(async (req, res) => {
 
+}))
 // TODO edit a comment
+router.put('/', commentValidation, asyncHandler(async (req, res) => {
+
+}))
 
 // TODO delete a comment
 // router.delete('/', asyncHandler(async (req, res) => {
 //     const comment = await Comment.findByPk(req.body.id);
 //     console.log('COMMENT', comment);
-//     await image.destroy();
+//     await comment.destroy();
 //     return res.json({ message: 'success' });
 // }));
 
