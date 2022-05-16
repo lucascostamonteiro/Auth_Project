2799f6c8fb3672342abeb8e669bd6ab72bef5012
Lucas <lucascostamonteiro@yahoo.com>
2022-05-16 12:51:22 -0700

    favorites done

--- frontend/src/components/MyFavorites/index.js
+++ frontend/src/components/MyFavorites/index.js
@@ -1,22 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
-// import { ModalImage } from '../../context/ImageModal';
-// import ImageDetail from '../ImageDetail';
 import SingleImage from "../SingleImage";
 
 
 function MyFavorites() {
   const allImages = useSelector(state => state.images);
   const userFavorites = Object.values(useSelector(state => state.favorites.user));
-  // const [showModal, setShowModal] = useState(false);
-
-  // const favorites = Object.values(useSelector(state => state.favorites));
-  console.log('FAVORITES', userFavorites)
-
-  // const handleImgError = (e) => {
-  //   e.target.src = '../../../../static/not-image.png';
-  // }
-
 
   return (
     <>
@@ -26,11 +15,6 @@ function MyFavorites() {
         :
         <>
           {userFavorites?.map(favorite => (
-            // <div key={favorite.id} className="favorite-main-div">
-            //   <h2>{allImages[favorite?.imageId]?.content}</h2>
-            //   <img className="single-image-favorite" src={allImages[favorite?.imageId]?.imageUrl} onError={handleImgError}
-            //   />
-            // </div>
             <SingleImage image={allImages[favorite?.imageId]} key={favorite.id} />
           ))}
         </>
